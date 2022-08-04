60fe9760dec85a7f731b8ea6dabf6bca9413d0de
Damian Borowiecki <dborowiecki@soldevelo.com>
2022-08-04 20:28:55 +0200

    TRAN-56: Code coverage report attached to sonar

--- transit/tests/__init__.py
+++ transit/tests/__init__.py
@@ -3,7 +3,11 @@ from django.test import TestCase
 
 class MockTestCase(TestCase):
     """
-    Created to validate tests configuration. Remove after any actual test are added.
+    Created to validate tests configuration and automatic builds. Remove after any actual test are added.
     """
     def test_true(self):
         self.assertTrue(True)
+
+    def test_functionality_coverage(self):
+        from transit.urls import urlpatterns
+        self.assertGreater(len(urlpatterns), 1)
