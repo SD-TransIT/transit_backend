d8e049a555ea7104e6be9de3401a679da6f66ce7
Damian Borowiecki <dborowiecki@soldevelo.com>
2022-08-26 16:01:07 +0200

    TRAN-82: Fix lazy formatting

--- scripts/create_superuser.py
+++ scripts/create_superuser.py
@@ -9,6 +9,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 login = getenv('SUPERUSER_LOGIN', None)
 passwd = getenv('SUPERUSER_PASSWORD', None)
+
 mail = getenv('SUPERUSER_EMAIL', None)
 
 if not (login and passwd):
@@ -19,9 +20,8 @@ User = get_user_model()
 exists = User.objects.filter(username=login).exists()
 
 if exists:
-    logging.info(F"Superuser with login {login} already exists. New user will not be created")
+    logging.info("Superuser with login %s already exists. New user will not be created", login)
     sys.exit()
 
 User.objects.create_superuser(login, mail, passwd)
-logging.info(F"Superuser with login {login} created from .env specification")
-
+logging.info("Superuser with login %s created from .env specification", login)
