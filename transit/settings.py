69a719f6fcef94800f88a27a656cc4ffde471a90
sniedzielski <52816247+sniedzielski@users.noreply.github.com>
2022-11-07 16:54:52 +0100

    hotfix: fixed env variable name in superset auth (#62)

--- transit/settings.py
+++ transit/settings.py
@@ -201,7 +201,7 @@ else:
 
 # environmental variable for Superset configuration
 SUPERSET_HOST = os.getenv('SUPERSET_HOST', 'http://localhost:8088/')
-SUPERSET_ADMIN = os.getenv('SUPERSET_PASSWORD', 'admin')
+SUPERSET_ADMIN = os.getenv('SUPERSET_ADMIN', 'admin')
 SUPERSET_PASSWORD = os.getenv('SUPERSET_PASSWORD', 'admin')
 SUPERSET_USERNAME_DASHBOARD = os.getenv('SUPERSET_USERNAME_DASHBOARD', 'apiUser')
 SUPERSET_FIRSTNAME_DASHBOARD = os.getenv('SUPERSET_FIRSTNAME_DASHBOARD', 'apiUser')
